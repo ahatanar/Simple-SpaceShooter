fb7dba8dbcae21356f0ee866b59a58636be61f78
ahatanar <103181988+ahatanar@users.noreply.github.com>
2022-05-22 20:10:29 -0400

    Update enemy.js

--- SpaceShooter/scripts/enemy.js
+++ SpaceShooter/scripts/enemy.js
@@ -7,23 +7,25 @@ var generaterocks = setInterval(() => {
    
     //place rock in a random location
     rock.style.left = Math.floor(Math.random() * 450) + "px";
-  
     board.appendChild(rock);
+  //every second 
   }, 1000);
   
   var moverocks = setInterval(() => {
     
     var rocks = document.getElementsByClassName("rocks");
-  
+  //if rocks do exist
     if (rocks != undefined) {
+      //putting rocks into an array
       for (var i = 0; i < rocks.length; i++) {
         var rock = rocks[i]; 
+        //seeing how far rock is from the top
         var rocktop = parseInt(
         window.getComputedStyle(rock).getPropertyValue("top")
         );
-        //if rock reaches end game over
+        //if rock reaches end
         if (rocktop >= 475) {
-          
+          //Game is over, remove the rock and reload the page
           alert("Game Over");
           rock.parentElement.removeChild(rock);
           clearInterval(moverocks);
